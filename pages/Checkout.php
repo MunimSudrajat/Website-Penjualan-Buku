79b590161cd588f17a83a7d5dc193eb0e6094414
FajarMaulana566 <muhfajar875@gmail.com>
2025-10-17 15:10:06 +0700

    footer co

--- pages/Checkout.php
+++ pages/Checkout.php
@@ -149,7 +149,7 @@ if (isset($_SESSION['cart'])) {
         </div>
     <?php endif; ?>
 </div>
-<?php include '../Includes/footer.php'; ?>
+<?php include './Includes/footer.php'; ?>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
